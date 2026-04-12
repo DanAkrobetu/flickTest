1a6dcc7de3703148afa10db18d95da0dc84e679a
Daniel Akrobetu <akrobetudaniel@gmail.com>
2026-04-12 17:47:40 -0500

    Better movement
    rewrote movement code myself and added in double jumping. I will be tweaking this though to make it more satisfying

--- Player/player.gd
+++ Player/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
+const maxSpeed: float = 500
+const maxJumpHeight: float = 500
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var doubleJump: bool = false
 
+func _ready() -> void:
+	velocity = Vector2.ZERO
+
+	 
+func get_input():
+	var inputDir = Input.get_axis("left", "right")
+	velocity.x = inputDir * maxSpeed
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		print("jump")
+		velocity.y -= maxJumpHeight
+	elif !is_on_floor() and !doubleJump and Input.is_action_just_pressed("jump"):
+		velocity.y -= maxJumpHeight
+		doubleJump = true
+		print("doube jump")
+	
+	#print("InputDir:" + str(inputDir))
+	#print("velocity.x:" + str(velocity.x))
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if !is_on_floor():
+		velocity += delta * get_gravity()
+	elif is_on_floor():
+		doubleJump = false
+	get_input()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 
 	move_and_slide()
